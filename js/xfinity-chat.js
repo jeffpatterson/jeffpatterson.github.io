78294c38a342a4dea7a9d91af7cccc0cd9feb04b
jeffpatterson <jeff@directly.com>
2019-03-18 12:34:54 -0700

    OfferUp update

--- js/xfinity-chat.js
+++ js/xfinity-chat.js
@@ -3,5 +3,5 @@ var new_element = old_element.cloneNode(true);
 old_element.parentNode.replaceChild(new_element, old_element);
 
 new_element.onclick = function() {
-    window.open('https://jeffpatterson.github.io/html/xfinity-mobile.html', '_blank', 'location=no,height=880,width=550,scrollbars=no,status=no');
+    window.open('https://jeffpatterson.github.io/html/xfinity-mobile.html', '_blank', 'location=no,height=880,width=550,scrollbars=no,status=no,top=139,left=290');
 }
