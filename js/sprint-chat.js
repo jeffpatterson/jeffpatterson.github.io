edaf167d2fcfad5968fc51c874056c08f9dedc6c
jeffpatterson <jeff@directly.com>
2019-04-29 21:25:14 -0700

    Adding Sprint

--- js/sprint-chat.js
+++ js/sprint-chat.js
@@ -3,6 +3,6 @@ setTimeout(function() { var old_element = document.getElementById("inqC2C4ImgCon
     old_element.parentNode.replaceChild(new_element, old_element);
 
     new_element.onclick = function() {
-        window.open('https://jeffpatterson.github.io/html/sprint-mobile.html', '_blank', 'location=no,height=850,width=420,scrollbars=no,status=no,top=139,left=290');
+        window.open('https://jeffpatterson.github.io/html/sprint-mobile.html', '_blank', 'location=no,height=850,width=420,scrollbars=no,status=no,top=139,left=709');
     }
 }, 5000);
