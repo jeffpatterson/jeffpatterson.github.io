1948b5337ba0c1bf56d213a27064bf00ef397d75
jeffpatterson <jeff@directly.com>
2019-05-27 11:22:46 -0700

    Adding BT

--- js/bt-chat.js
+++ js/bt-chat.js
@@ -3,6 +3,6 @@ setTimeout(function() { var old_element = document.getElementsByClassName("chat"
     old_element.parentNode.replaceChild(new_element, old_element);
 
     new_element.onclick = function() {
-        window.open('https://jeffpatterson.github.io/html/xfinity-mobile.html', '_blank', 'location=no,height=850,width=520,scrollbars=no,status=no,top=139,left=290');
+        window.open('https://jeffpatterson.github.io/html/bt-mobile.html', '_blank', 'location=no,height=850,width=520,scrollbars=no,status=no,top=139,left=290');
     }
 }, 5000);
